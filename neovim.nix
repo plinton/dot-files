fe997af94af9a44d2a39fb9e81b378e8371470f4
Paul Ellis Linton <plinton@musicalcomputer.com>
2022-07-26 15:14:19 +0200

    Fix issue with plenary and gitsigns not being found by moving them to extraLuaPackages, remove unused nvim-compe

--- neovim.nix
+++ neovim.nix
@@ -8,7 +8,10 @@ in
     enable = true;
     package = pkgsUnstable.neovim-unwrapped;
     withNodeJs = true;
+    # The neovim ruby overrides the one specified in the shell. Great for plugins in ruby, but breaks sorbet's lookups
     withRuby = false;
+    # Put some lua-based plugins here as sometimes runtimepath does not always pick them up
+    extraLuaPackages = with pkgsUnstable.lua51Packages; [ plenary-nvim gitsigns-nvim ];
     extraConfig = "lua <<EOF\n" + builtins.readFile ./init.lua + "EOF\n";
     extraPackages = with pkgsUnstable; [
       nodePackages.typescript-language-server
@@ -16,7 +19,6 @@ in
     ];
     plugins = with pkgsUnstable.vimPlugins; [
       gitsigns-nvim
-      plenary-nvim
       nvim-lspconfig
       vim-sensible
       vim-surround
@@ -28,7 +30,6 @@ in
       vim-illuminate
       split-term-vim
       vim-sleuth
-      nvim-compe
       nvim-web-devicons
       lualine-nvim
       (nvim-treesitter.withPlugins (plugins: pkgsUnstable.tree-sitter.allGrammars))
