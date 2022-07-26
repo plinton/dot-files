87f5794f8308037f89548743d24e0eb3455c3191
Paul Ellis Linton <plinton@musicalcomputer.com>
2022-07-26 14:19:22 +0200

    add catppuccin colors to fzf

--- terminal.nix
+++ terminal.nix
@@ -33,6 +33,11 @@
     enable = true;
     enableZshIntegration = true;
     defaultCommand = "fd --type f";
+    defaultOptions = [
+      "--color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284"
+      "--color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf"
+      "--color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284"
+    ];
     fileWidgetCommand = "fd --type f";
     fileWidgetOptions = [ "--preview 'head {}'"];
   };
