56462e47c5ca46a8072e45ac0059c15063eb4dfc
Paul Ellis Linton <plinton@musicalcomputer.com>
2022-08-12 01:32:43 +0200

    Fetch and use catppuccin colorscheme for kitty

--- kitty.nix
+++ kitty.nix
@@ -1,9 +1,17 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
+let
+in
 {
   programs.kitty = {
     enable = true;
     settings = {
       shell = "zsh";
     };
+    extraConfig = builtins.readFile (pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "kitty";
+            rev = "ad38e5bb1b1ab04e7d2cf86ded289c455df62908";
+            sha256 = "0vb5fkpxjyyj180wfc948c1qvndlcwv0mzmz0xdv7wdg7qj9v7hk";
+  } + "/mocha.conf");
   };
 }
